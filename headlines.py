d44a4432d791557dd949cbeffcc24210af0ed778
tabshi <tabshi@outlook.com>
2016-10-24 11:45:43 +0800

    section5

--- headlines.py
+++ headlines.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-import feedparser
-import json
-import urllib2
-import urllib
-import datetime
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 from flask import Flask
+from flask import make_response
 from flask import render_template
 from flask import request
-from flask import make_response
+import datetime
+import feedparser
+import json
+import sys
+import urllib
+import urllib2
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
 app = Flask(__name__)
@@ -33,20 +33,12 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 
 @app.route('/')
 def home():
-    publication = request.args.get('publication')
-    if not publication:
-        publication = DEFAULTS['publication']
+    publication = get_value_with_fallback('publication')
     articles = get_news(publication)
-    city = request.args.get('city')
-    if not city:
-        city = DEFAULTS['city']
+    city = get_value_with_fallback('city')
     weather = get_weather(city)
-    currency_from = request.args.get('currency_from')
-    if not currency_from:
-        currency_from = DEFAULTS['currency_from']
-    currency_to = request.args.get('currency_to')
-    if not currency_to:
-        currency_to = DEFAULTS['currency_to']
+    currency_from = get_value_with_fallback('currency_from')
+    currency_to = get_value_with_fallback('currency_to')
     rate, currencies = get_rate(currency_from,currency_to)
     response = make_response(render_template("home.html",
                                              articles=articles,
@@ -61,6 +53,13 @@ def home():
     response.set_cookie("currency_from",currency_from,expires=expires)
     response.set_cookie("currency_to",currency_to,expires=expires)
     return response
+
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
 
 def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
